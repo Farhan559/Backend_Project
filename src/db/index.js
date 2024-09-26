488aacf8e2967d6e73af539e99186bd78fd1be0b
Farhan559 <farhansiddiqui559@gmail.com>
2024-09-26 18:01:47 +0500

    feat: connect to mongo atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "./contants";
+import dotenv from "dotenv"; // Ensure dotenv is imported
+import { DB_NAME } from "../contants.js";
 
-(async()=>{
+// Load environment variables
+dotenv.config();
+
+const connectDB = async () => {
     try {
-            mongoose.connect(`${process.env.DATABASE_URI}/${DB_NAME}`)
-            console.log(`\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(process.env.DATABASE_URI);
+        console.log(`\nMongoDB Connected!! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.error("ERROR: ",error);
-        throw err
+        console.error("MONGODB CONNECTION ERROR", error);
+        process.exit(1); // Exit the process on error
     }
-})
+};
+
+export default connectDB;
