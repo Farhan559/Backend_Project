352fb80d9e2c63c135433e38095880124f7eabe2
Farhan559 <farhansiddiqui559@gmail.com>
2024-09-30 18:41:06 +0500

    feat: Logged Out

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -14,6 +15,11 @@ router.route("/register").post(
                         maxCount:1
                     }
                 ]),
-    registerUser)
+    registerUser
+)
+router.route("/login").post(loginUser)
+
+// secured Routes
+router.route("/logout").post(verifyJWT,logOutUser)
 
 export default router;
