2b99338e4d61a6d5b4a31035d8294122bef4ed3a
Farhan559 <farhansiddiqui559@gmail.com>
2024-09-30 18:40:05 +0500

    feat: logged out

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -118,8 +118,29 @@ const registerUser = asyncHandler(async(req,res)=>{
             "User logged in Successfully"
         )
        )
+     });
+    //  LogOut
+    const logOutUser = asyncHandler(async(req,res)=>{
+       await User.findByIdAndUpdate(
+            req.user._id,{
+                $set:{
+                    refreshToken:undefined
+                }
+            },{
+                new:true
+            }
+        )
+        const options = {
+            httpOnly:true,
+            secure:true
+           }
+           return res
+           .status(200)
+           .clearCookie("accessToken",options)
+           .clearCookie("refreshToken",options)
+           .json(new ApiResponse(200,{},"user logged Out"))
 
     })
 
 
-export {registerUser , loginUser}
+export {registerUser , loginUser , logOutUser}
